acb111410a31db9b2d12407eddd7d933da68ed92
John Paul Tolentino <jpftolentino@gmail.com>
2025-10-24 03:47:09 -0400

    Updated the review file to have proper comments

--- PhaseOne/week2/week2review.py
+++ PhaseOne/week2/week2review.py
@@ -1,32 +1,32 @@
-🔹 Basics & Control Flow
-#R1. Check if a number is even or odd.
-Example: 7 → odd
+# 🔹 Basics & Control Flow
+# R1. Check if a number is even or odd.
+# Example: 7 → odd
 
-#R2. Print all numbers from 1 to 10 except multiples of 3.
-Example: Output → 1 2 4 5 7 8 10
+# R2. Print all numbers from 1 to 10 except multiples of 3.
+# Example: Output → 1 2 4 5 7 8 10
 
-#R3. Given a number, print whether it’s positive, negative, or zero.
-Example: -5 → negative
+# R3. Given a number, print whether it’s positive, negative, or zero.
+# Example: -5 → negative
 
-#R4. Write a loop that sums all numbers from 1 to n.
-Example: n = 5 → 15
+# R4. Write a loop that sums all numbers from 1 to n.
+# Example: n = 5 → 15
 
-#R5. Check if a number is a multiple of 5 and 7.
-Example: 35 → True
+# R5. Check if a number is a multiple of 5 and 7.
+# Example: 35 → True
 
-🔹 Strings & Lists
+# 🔹 Strings & Lists
 
-#R6. Reverse a string manually (no slicing).
-Example: "Python" → "nohtyP"
+# R6. Reverse a string manually (no slicing).
+# Example: "Python" → "nohtyP"
 
-#R7. Count how many vowels are in a given string.
-Example: "education" → 5
+# R7. Count how many vowels are in a given string.
+# Example: "education" → 5
 
-#R8. Replace every space in a string with a dash (-).
-Example: "hello world" → "hello-world"
+# R8. Replace every space in a string with a dash (-).
+# Example: "hello world" → "hello-world"
 
-#R9. Find the largest number in a list.
-Example: [5, 9, 2, 8] → 9
+# R9. Find the largest number in a list.
+# Example: [5, 9, 2, 8] → 9
 
-#R10. Remove duplicates from a list while keeping order.
-Edasdxample: [1, 2, 2, 3, 1] → [1, 2, 3]
+# R10. Remove duplicates from a list while keeping order.
+# Example: [1, 2, 2, 3, 1] → [1, 2, 3]
