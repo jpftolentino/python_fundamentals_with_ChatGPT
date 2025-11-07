7a3ae6d4975da6defdc7e4fd7f734d05a449962e
John Paul Tolentino <jpftolentino@gmail.com>
2025-11-07 06:40:17 -0500

    Created Week 3 Day 2 file to start problem set

---
+++ PhaseOne/week3/day2.py
@@ -0,0 +1,55 @@
+# 🔹 Week 3 — Day 2: Dictionaries & Sets (Problems 111–120)
+
+# #111
+# Create a set of unique numbers from a list.
+# Input: [1, 2, 2, 3, 1, 4]
+# Output: {1, 2, 3, 4}
+
+unique_num = set([1, 2, 2, 3, 1, 4])
+
+print(unique_num)
+
+# #112
+# Add a new element to a set.
+# Input: {1, 2, 3}, element = 4
+# Output: {1, 2, 3, 4}
+
+# #113
+# Remove an element from a set if it exists.
+# Input: {1, 2, 3, 4}, remove 3
+# Output: {1, 2, 4}
+
+# #114
+# Check if an element exists in a set.
+# Input: {1, 2, 3}, check 5
+# Output: False
+
+# #115
+# Find the union of two sets.
+# Input: {1, 2}, {2, 3}
+# Output: {1, 2, 3}
+
+# #116
+# Find the intersection of two sets.
+# Input: {1, 2, 3}, {2, 3, 4}
+# Output: {2, 3}
+
+# #117
+# Find the difference between two sets.
+# Input: {1, 2, 3}, {2, 3, 4}
+# Output: {1}
+
+# #118
+# Check if one set is a subset of another.
+# Input: {1, 2}, {1, 2, 3}
+# Output: True
+
+# #119
+# Convert a string into a set of unique characters.
+# Input: "hello"
+# Output: {'h', 'e', 'l', 'o'}
+
+# #120
+# Get the number of unique items in a list using a set.
+# Input: [1, 2, 2, 3, 4, 4]
+# Output: 4
